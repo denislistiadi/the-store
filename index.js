3514bed972c4134cd71433417d28e3d0e0dc2803
Denis Listiadi <denislistiadi24@gmail.com>
2022-05-11 11:46:23 +0700

    listening-changes

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 function createStore() {
-    // The Store should have four methods:
-    // 1. The state
-    // 2. Get the state
-    // 3. Listen to changes on the state
-    // 4. Update the state
+  // The Store should have four methods:
+  // 1. The state
+  // 2. Get the state
+  // 3. Listen to changes on the state
+  // 4. Update the state
 
-    let state;
+  let state;
+  let listeners = [];
 
-    const getState = () => state;
+  const getState = () => state;
 
-    return {
-        getState
-    }
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  return {
+    getState,
+    subscribe,
+  };
 }
